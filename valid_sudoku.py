2acf603599185e87424d12f45f06fdb27c7a4abe
Nep T. Orshiso <nep.orshiso@gmail.com>
2019-12-24 19:08:14 -0700

    2 of 3 conditions passing

---
+++ valid_sudoku.py
@@ -0,0 +1,76 @@
+'''
+Source: 
+Problem: Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+    1. Each row must contain the digits 1-9 without repetition.
+    2. Each column must contain the digits 1-9 without repetition.
+    3. Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
+'''
+
+input_one = [
+    ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+]
+
+input_two = [
+    ["8", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+]
+
+# this one tests condition 3
+input_three = [
+    [".",".",".",".","5",".",".","1","."],
+    [".","4",".","3",".",".",".",".","."],
+    [".",".",".",".",".","3",".",".","1"],
+    ["8",".",".",".",".",".",".","2","."],
+    [".",".","2",".","7",".",".",".","."],
+    [".","1","5",".",".",".",".",".","."],
+    [".",".",".",".",".","2",".",".","."],
+    [".","2",".","9",".",".",".",".","."],
+    [".",".","4",".",".",".",".",".","."]
+]
+
+
+def isValidSudoku(board):
+    # this was used in a previous submission. Modified for this example.
+    def duplicate_detection(list):
+        tracker = {}
+        list = [item for item in list if item != "."]
+        for i in list:
+            if i not in tracker:
+                tracker[i] = 1
+            else:
+                return True
+
+    # checks for duplicates across a row
+    for row in board:
+        row_detection = duplicate_detection(row)
+        if row_detection == True:
+            return False
+
+    # uses list comprehension to isolate columns
+    for i in range(len(board)):
+        col = [row[i] for row in board]
+        col_detection = duplicate_detection(col)
+        if col_detection == True:
+            return False
+
+    return True
+
+
+print(isValidSudoku(input_two))
